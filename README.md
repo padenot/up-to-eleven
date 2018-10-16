7435640a00c638b3a5b36c1607b30eb70891aa55
Paul Adenot <paul@paul.cx>
2018-10-16 13:58:13 +0200

    enable/disable + readme + icon change

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # Up To Eleven
 
 A Web Extension that inserts a Web Audio API `GainNode` and `DynamicsCompressor`
-node to improve the audio of videos that are too quiet.
+node to increase the audio level of videos that are too quiet, while limiting
+the maximum volume of those videos.
+
+A few examples:
+- https://www.youtube.com/watch?v=23lRkdDXqY0
+- https://www.youtube.com/watch?v=FJoYZUMOwM4
 
 This is a proof of concept: the input gain to the compressor is fixed, and the
-compressor parameters are fixed as well.
+compressor parameters are fixed as well. A byproduct of this technique is that
+the background noise is going to be increased.
 
-- Firefox: <https://addons.mozilla.org/en-US/firefox/addon/up-to-eleven/>
+- Firefox: <https://addons.mozilla.org/firefox/addon/up-to-eleven/>
 - Chrome: It works but I haven't bothered uploading it to their site, let me know
 if anybody is interested
 
--- up-to-eleven.js
+++ up-to-eleven.js
@@ -1,22 +1,55 @@
-var browser = browser || chrome
+var browser = browser || chrome;
+var enabled = false;
 
 function doit(e) {
-  browser.tabs.executeScript({
-      code: `
-        var element = document.querySelector("video");
-        var ac = new AudioContext();
-        // brick-wall limiter to avoid output saturation.
-        // This one has a makeup gain stage built-in
-        var comp = new DynamicsCompressorNode(ac, {ratio: 20.0});
-        // Increase the gain quite a lot. Maybe it would be good to make it
-        // configurable.
-        var gain = new GainNode(ac, {gain: 10.0});
-        var source = new MediaElementAudioSourceNode(ac, { mediaElement:
-                                                           element });
-        source.connect(gain).connect(comp).connect(ac.destination);
-        undefined;
-    `
-  });
+  if (enabled) {
+    browser.browserAction.setIcon({
+      path: {
+        48: "icons/uptoeleven-disabled.png",
+        96: "icons/uptoeleven-disabled.png"
+      }
+    })
+    browser.tabs.executeScript({
+        code: `
+          window.__source.disconnect();
+          window.__source.connect(window.__ac.destination);
+          undefined;
+      `
+    });
+  } else {
+    browser.browserAction.setIcon({
+      path: {
+        48: "icons/uptoeleven-enabled.png",
+        96: "icons/uptoeleven-enabled.png"
+      }
+    })
+    browser.tabs.executeScript({
+        code: `
+          if (!window.__ac) {
+            window.__ac = new AudioContext();
+          }
+
+          // brick-wall limiter to avoid output saturation.
+          // This one has a makeup gain stage built-in
+          var comp = new DynamicsCompressorNode(window.__ac, {ratio: 20.0, threshold: -50});
+
+          // Increase the gain quite a lot. Maybe it would be good to make it
+          // configurable.
+          var gain = new GainNode(window.__ac, {gain: 10.0});
+
+          if (!window.__source) {
+            var element = document.querySelector("video");
+            window.__source = new MediaElementAudioSourceNode(window.__ac, { mediaElement:
+                                                                    element});
+          } else {
+            window.__source.disconnect();
+          }
+          window.__source.connect(gain).connect(comp).connect(window.__ac.destination);
+          undefined;
+      `
+    });
+  }
+  enabled = !enabled;
 }
 
 browser.browserAction.onClicked.addListener(doit);
